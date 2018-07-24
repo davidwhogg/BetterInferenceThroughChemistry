d3cf9d821ac689da40a2c4b0f9621dba20a5dea4
davidwhogg <david.hogg@nyu.edu>
2018-07-24 06:15:18 +0200

    more testing

--- py/integrate_orbits.py
+++ py/integrate_orbits.py
@@ -6,7 +6,7 @@ def leapfrog_step(z, v, dt, acceleration, pars):
     return znew, v + 0.5 * dv, v + dv
 
 def leapfrog(vmax, dt, acceleration, pars):
-    maxstep = 32768
+    maxstep = 32768 * 16
     zs = np.zeros(maxstep) - np.Inf
     vs = np.zeros(maxstep) - np.Inf
     zs[0] = 0.
@@ -16,16 +16,21 @@ def leapfrog(vmax, dt, acceleration, pars):
         zs[t + 1], vs[t + 1], v = leapfrog_step(zs[t], v, dt, acceleration, pars)
         if zs[t] < 0. and zs[t+1] >= 0.:
             break
-    return zs[:t], vs[:t]
+    return zs[:t+2], vs[:t+2]
 
 def dummy(z, pars):
-    return -1. * np.sign(z)
+    return -1.5 * np.sign(z)
 
 if __name__ == "__main__":
     import pylab as plt
 
     pars = None
-    zs, vs = leapfrog(100., 0.1, dummy, pars)
+    vmax = 25.
+    zs, vs = leapfrog(vmax, 0.01, dummy, pars)
     print(len(zs), len(vs), min(zs))
-    plt.plot(vs, zs, "k.")
+    print(zs[-3:], vs[-3:], min(zs))
+    plt.plot(vs, zs, "k-")
     plt.savefig("eatme.png")
+    plt.xlim(vmax-1., vmax+1.)
+    plt.ylim(-1., 1.)
+    plt.savefig("biteme.png")
