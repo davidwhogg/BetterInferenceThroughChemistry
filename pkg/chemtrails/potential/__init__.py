f863fb919c216cf6789962f862eb043332a93a84
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-08-29 09:41:58 -0600

    initial checkin

---
+++ pkg/chemtrails/potential/__init__.py
@@ -0,0 +1,1 @@
+from .core import Sech2DensityPotential, UniformDensityPotential
---
+++ pkg/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='chemtrails',
+      version='0.1',
+      description='TODO',
+      install_requires=['numpy', 'astro-gala'],
+      author='adrn',
+      author_email='adrn@astro.princeton.edu',
+      license='MIT',
+      url='https://github.com/davidwhogg/ChemicalTangents',
+      packages=["chemtrails", "chemtrails.potential"])
