4c35dbed4c750faf9256296c6978cd2ba898586d
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-08-30 13:26:40 -0600

    compare energy and likelihood

--- pkg/chemtrails/tests/test_likelihood.py
+++ pkg/chemtrails/tests/test_likelihood.py
@@ -20,6 +20,7 @@ def test_compare_hogg():
     if _path not in sys.path:
         sys.path.append(_path)
     from integrate_orbits import paint_energies
+    from chemical_tangents import ln_like
 
     # Load a subset of the data
     galah_path = os.path.join(os.path.split(os.path.abspath(__file__))[0],
@@ -48,3 +49,10 @@ def test_compare_hogg():
     E_dwh = paint_energies(atzs, atvs, sunpars, dynpars) * u.m**2/u.s**2
 
     assert quantity_allclose(E_apw, E_dwh)
+
+    # now check likelihood evaluation
+    qs = model.element_data['fe_h']
+    ll_apw = model.ln_metal_likelihood(qs, E_apw.value)
+    ll_dwh = ln_like(qs, E_dwh.to(E_apw.unit).value,
+                     order=model.metals_deg)
+    assert quantity_allclose(ll_apw, ll_dwh)
