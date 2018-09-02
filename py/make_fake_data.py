b6b07c5a475ea8d67161ac4f8f3a1d044c38f1bb
davidwhogg <david.hogg@nyu.edu>
2018-09-01 20:47:46 -0700

    something happens

--- py/make_fake_data.py
+++ py/make_fake_data.py
@@ -19,18 +19,19 @@ def make_one_fake_star(pars):
     - not adding noise to anything
     """
     # unpack pars
-    foo, a, b, c, sunpars, dynpars = pars
+    foo, a, b, c, acceleration, sunpars, dynpars = pars
     # choose x,y
-    x, y = np.random.uniform(-1000., 1000., size=2) # pc
-    # choose Ez and metallicity
+    x, y = np.random.uniform(-1000. * pc, 1000. * pc, size=2) # pc
+    # choose Ez isothermally
     lnEz = foo + np.log(np.random.normal() ** 2)
+    # choose metallicity by a polynomial
     abundnace = a + b * lnEz + c * lnEz * lnEz + 0.25 * np.random.normal()
     # choose an angle
     phi_z = 2. * np.pi * np.random.uniform()
     # integrate
-    v0 = np.sqrt(2. * np.exp(lnEz))
-    z, vz = integrate_to_phi(phi_z, v0)
-    return x, y, z, vz
+    v0 = np.sqrt(2. * np.exp(lnEz)) * km / s # woot
+    z, vz = integrate_to_phi(phi_z, v0, 0.1 * Myr, acceleration, dynpars)
+    return x, y, z - sunpars[0], vz - sunpars[1]
 
 def selection_function(x, y, z):
     """
@@ -38,14 +39,20 @@ def selection_function(x, y, z):
 
     bugs:
     -----
-    - need to add sky cuts
+    - need to make sky cut work!
     """
-    if x * x + y * y + z * z > 1000. * 1000.:
+    # not far away
+    if x * x + y * y + z * z > (1000. * pc) ** 2:
         return 0.
+    # not in the Galactic plane
+    if z * z / (x * x + y * y) < np.tan(20. * np.pi / 180.) ** 2:
+        return 0.
+    # not way North
+#    if x * xhat + y * yhat + z * zhat > something:
+#        return 0.
     return 1.
 
-def make_catalog():
-    N = 5000
+def make_catalog(N, pars):
     n = 0
     poss, vzs = np.zeros((N, 3)), np.zeros(N)
     while n < N:
@@ -55,7 +62,14 @@ def make_catalog():
             poss[n, :] = x, y, z
             vzs[n] = vz
             n += 1
+    return poss, vzs
 
 def write_catalog():
     whatever
     return None
+
+if __name__ == "__main__":
+
+    pars = (5., 0., -0.5, -0.1, sech_squared_acceleration, [10. * pc, 1. * (km / s)], [65. * sigunits, 350. * pc])
+    poss, vzs = make_catalog(16, pars)
+    print(poss[:, 2] / pc, vzs / (km / s))
